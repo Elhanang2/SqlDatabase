e739074b5db33a5f7be382a08c375114e19a1fc0
Ayna <aynazewd@yahoo.com>
2019-07-22 13:10:18 -0700

    update server.js

--- server.js
+++ server.js
@@ -39,11 +39,16 @@ app.use(passport.initialize());
 //passport config
 require("./config/passport")(passport);
 
-app.use(express.static(path.join(__dirname, "..", "client", "build")))
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
-app.get('*', (req, res) => {
-  res.sendFile( path.join(__dirname, "..", "client", "build", "index.html"))
-})
 
 // launch our backend into a port
 app.listen(port, () => console.log(`Server running on port ${port} !`))
