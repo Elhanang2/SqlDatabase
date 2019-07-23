c6df1ca5ba983c51db72e8d909e3e55f5445c0e5
Ayna <aynazewd@yahoo.com>
2019-07-23 10:22:50 -0700

    update server.js

--- server.js
+++ server.js
@@ -16,8 +16,14 @@ app.use(
 
 app.use(bodyParser.json());
 
+app.use('/', express.static(path.join(__dirname, '/client/build')));
+
 //Routes
 app.use("/api/users", users);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+  });
 
 //DB Config
 const db = require("./config/keys").mongoURI;
@@ -39,15 +45,6 @@ app.use(passport.initialize());
 //passport config
 require("./config/passport")(passport);
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
 
 
 // launch our backend into a port
